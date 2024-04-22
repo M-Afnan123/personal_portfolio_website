3ca5f14101a861ff76383708c22d8fd6698a9dcc
Mohammed Afnan Z <108898065+M-Afnan123@users.noreply.github.com>
2024-04-23 00:54:23 +0530

    Update style.js

--- style.js
+++ style.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing",{
-		strings: ["Aspiring Software Developer ", "Coding Enthusiast", " Front-end Developer"],
+		strings: ["Software Engineer ", "Coding Enthusiast", " Front-end Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Aspiring Software Developer", "Coding Enthusiast", "Front-end Developer"],
+        strings: ["Software Engineer", "Coding Enthusiast", "Front-end Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
